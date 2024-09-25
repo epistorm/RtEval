5414d24ab1544a54b897c31f95407580e8e34bb9
Kaitlyn Johnson <94390107+kaitejohnson@users.noreply.github.com>
2024-09-25 11:46:01 -0400

    Fix  `EpiNow2` delay so that it is infection to report not onset to report (#25)
    * fix delay so that it is infection to report not onset to report
    
    * Remove dependence on epinowcast
    
    ---------
    
    Co-authored-by: athowes <adamthowes@gmail.com>

--- 02_EPINOW2_report.R
+++ 02_EPINOW2_report.R
@@ -26,14 +26,31 @@ any(is.na(incidence_df))
 
 ####
 gi_pmf <- NonParametric(pmf = all_data$serial$Px)
-delay_pmf <- NonParametric(pmf = all_data$reporting_delay$Px)
+# Delay is symptom onset to report, therefore need to convolve with
+# incubation to get the delay from infection to report
+sym_report_delay_pmf <- all_data$reporting_delay$Px
+incubation_pmf <- all_data$incubation$Px
 
-## ******
-## Note for Sam -- how to incorporate this?
-incubation_pmf <- NonParametric(pmf = all_data$incubation$Px)
-## *****
+pmfs <- list(
+  "incubation_period" = incubation_pmf,
+  "sym_report_delay_pmf" = sym_report_delay_pmf
+)
+to_simplex <- function(vector) {
+  return(vector / sum(vector))
+}
 
-#
+diff <- length(incubation_pmf) - length(sym_report_delay_pmf)
+stopifnot(diff >= 0)
+
+delay_pmf <- stats::convolve(
+    incubation_pmf,
+    rev(c(sym_report_delay_pmf, rep(0, diff))),
+    type = "open"
+  ) |>
+  to_simplex() |>
+  head(n = length(incubation_pmf) + length(sym_report_delay_pmf)) |>
+  NonParametric()
+
 res_epinow <- epinow(
   data = incidence_df,
   generation_time = generation_time_opts(gi_pmf),
@@ -52,16 +69,11 @@ dim(y_extract)
 # head(y_date)
 # View(res_epinow$samples)
 
-##
-# * including this since i havent figured out how to do it above
-INCUBATION_SHIFT = round(weighted.mean(x = all_data$incubation$Day,
-                                       w = all_data$incubation$Px))
-##
 
 
 plot_data <- data.frame(
   package = "EpiNow2",
-  date = c(all_data$cases$day, max(all_data$cases$day) + 1:7) - INCUBATION_SHIFT,
+  date = c(all_data$cases$day, max(all_data$cases$day) + 1:7),
   Rt_median = apply(y_extract, 2, quantile, probs = 0.5),
   Rt_lb = apply(y_extract, 2, quantile, probs = 0.025),
   Rt_ub = apply(y_extract, 2, quantile, probs = 0.975)
