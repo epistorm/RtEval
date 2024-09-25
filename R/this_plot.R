17351ce5415a25082e9df9418d71c95123a75aef
Connor Van Meter <31194265+connor-vanmeter@users.noreply.github.com>
2024-09-25 12:10:45 -0400

    Issue #16: Create epiestim_vignette.Rmd (#27)
    * Create epiestim_vignette.Rmd
    
    * Add :: to packages in plot
    
    * Edits to EpiEstim vignette
    
    ---------
    
    Co-authored-by: Connor Van Meter <connor.vanmeter@emory.edu>
    Co-authored-by: athowes <adamthowes@gmail.com>

--- R/this_plot.R
+++ R/this_plot.R
@@ -1,6 +1,6 @@
 this_plot <- function(pd, title) {
   as_tibble(pd) %>%
-    ggplot() + theme_classic2() +
+    ggplot() +
     geom_hline(yintercept = 1, color = 'grey') +
     geom_ribbon(aes(x = date, ymin = Rt_lb, ymax = Rt_ub,
                     fill = package), alpha = 0.25,show.legend = F) +
@@ -15,13 +15,13 @@ this_plot <- function(pd, title) {
     geom_line(aes(x = Day, y = Rt_calc), data = all_data$rt,
               linewidth= 0.5,
               linetype = '11') +
-    facet_rep_wrap(~package, repeat.tick.labels = 'y', nrow = 3) +
+    lemon::facet_rep_wrap(~package, repeat.tick.labels = 'y', nrow = 3) +
     # *******
     theme(
       axis.text = element_text(size = 10),
       axis.title = element_text(size = 14),
       strip.background = element_blank(),
       strip.text = element_text(face = 'bold'),
-      plot.title = element_markdown()
+      plot.title = ggtext::element_markdown()
     ) + ggtitle(title)
 }
