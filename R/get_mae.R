00a576c8c3a5dd86ed9f89d560319c479a63cc79
Laura F White <lfwhite@bu.edu>
2024-09-26 09:35:35 -0400

    initial commit
    function to calculate mean absolute error

---
+++ R/get_mae.R
@@ -0,0 +1,10 @@
+######################################################
+## FUNCTION TO CALCULATE THE MAE FOR VECTOR OF RT   ##
+######################################################
+get_mae(pd,rt_sim){
+  # pd is summarize.rtestimate object
+  # rt_sim is true values of rt
+
+  return(mean(abs(pd$estimates$median-rt_sim$Rt_calc)))
+
+}
